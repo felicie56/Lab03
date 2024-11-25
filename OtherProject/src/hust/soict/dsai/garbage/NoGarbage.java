452a68391514fa32ee974b26689480f92b5322da
felicie56 <146998878+felicie56@users.noreply.github.com>
2024-11-26 02:44:46 +0700

    Update NoGarbage.java

--- OtherProject/src/hust/soict/dsai/garbage/NoGarbage.java
+++ OtherProject/src/hust/soict/dsai/garbage/NoGarbage.java
@@ -1,22 +1,22 @@
-package OtherProjects.hust.soict.dsai.garbage;
+package hust.soict.dsai.garbage;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class NoGarbage {
-    public static void main(String[] args) throws Exception {
-        String filename = "C:\\Users\\ADMIN\\Downloads\\Documents\\large_file.txt"; // Đường dẫn file lớn
-        byte[] inputBytes = Files.readAllBytes(Paths.get(filename));
+    public static void main(String[] args) throws IOException {
+        String filename = "test.txt";
+        byte[] inputBytes = { 0 };
+        long startTime, endTime;
 
-        long startTime = System.currentTimeMillis();
-        StringBuilder outputStringBuilder = new StringBuilder();
-        for (byte b : inputBytes) {
-            outputStringBuilder.append((char) b);
+        inputBytes = Files.readAllBytes(Paths.get(filename));
+        startTime = System.currentTimeMillis();
+        StringBuilder outputStringBuilder = new StringBuilder("");
+        for(byte b : inputBytes) {
+            outputStringBuilder.append((char)b);
         }
-        @SuppressWarnings("unused")
-        String outputString = outputStringBuilder.toString();
-        long endTime = System.currentTimeMillis();
-
-        System.out.println("Time taken with StringBuilder: " + (endTime - startTime) + " ms");
+        endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
     }
 }
