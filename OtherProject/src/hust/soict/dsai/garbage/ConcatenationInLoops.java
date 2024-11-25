09b08348dceb21b47b8771cb8ce7160c85eaf997
felicie56 <146998878+felicie56@users.noreply.github.com>
2024-11-26 02:42:13 +0700

    Update ConcatenationInLoops.java

--- OtherProject/src/hust/soict/dsai/garbage/ConcatenationInLoops.java
+++ OtherProject/src/hust/soict/dsai/garbage/ConcatenationInLoops.java
@@ -1,22 +1,26 @@
-package hust.soict.dsai.garbage;
+package OtherProjects.hust.soict.dsai.garbage;
 
-import static java.lang.System.currentTimeMillis;
-import java.util.Random; //Lam cho code ngan gon hon thay vi goi nguyen phuong thuc ay
+import java.util.Random;
 
 public class ConcatenationInLoops {
     public static void main(String[] args) {
         Random r = new Random(123);
-        long start = currentTimeMillis();
+
+        long start = System.currentTimeMillis();
+        @SuppressWarnings("unused")
         String s = "";
-        for (int i = 0;i < 65536;i++) s += r.nextInt(2);
-        System.out.println(currentTimeMillis() - start);
+        for (int i = 0; i < 65536; i++) {
+            s += r.nextInt(2);
+        }
+        System.out.println("String concatenation: " + (System.currentTimeMillis() - start) + " ms");
 
         r = new Random(123);
         start = System.currentTimeMillis();
         StringBuilder sb = new StringBuilder();
-        for(int i = 0;i < 65536;i++)
+        for (int i = 0; i < 65536; i++) {
             sb.append(r.nextInt(2));
-        s += sb.toString();
-        System.out.println(System.currentTimeMillis() - start);
+        }
+        System.out.println("StringBuilder concatenation: " + (System.currentTimeMillis() - start) + " ms");
+
     }
 }
